2342c51f7a7fc3960b3c6e3bc66ea74e4a1a0930
Will Harris <will@harris.ch>
2024-12-28 22:30:42 +0000

    Migrate to python 3 and pyyaml 6

--- add_header_milter.py
+++ add_header_milter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import argparse
 import logging
@@ -9,9 +9,41 @@ import sys
 import time
 
 import libmilter
-import yaml
+
+from functools import wraps
+from typing import Any
+
+from yaml import load, Loader
 
 logger = logging.getLogger('AddHeaderMilter')
+
+
+def handle_bytes(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        def convert_bytes_recursive(obj: Any) -> Any:
+            if isinstance(obj, bytes):
+                return obj.decode('utf-8')
+            elif isinstance(obj, list):
+                return [convert_bytes_recursive(item) for item in obj]
+            elif isinstance(obj, dict):
+                return {
+                    convert_bytes_recursive(key): convert_bytes_recursive(value)
+                    for key, value in obj.items()
+                }
+            elif isinstance(obj, tuple):
+                return tuple(convert_bytes_recursive(item) for item in item)
+            elif isinstance(obj, set):
+                return {convert_bytes_recursive(item) for item in obj}
+            return obj
+
+        new_args = [convert_bytes_recursive(arg) for arg in args]
+        new_kwargs = {
+            convert_bytes_recursive(key): convert_bytes_recursive(value)
+            for key, value in kwargs.items()
+        }
+        return func(*new_args, **new_kwargs)
+    return wrapper
 
 
 def configure_logging(level=None, config_file=None):
@@ -19,7 +51,7 @@ def configure_logging(level=None, config_file=None):
     logger.setLevel(level)
     syslog_handler = logging.handlers.SysLogHandler(address='/dev/log', facility=logging.handlers.SysLogHandler.LOG_MAIL)
     syslog_handler.setLevel(level)
-    formatter = logging.Formatter('%(name)s[%(process)d]: %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(name)s[%(process)d]: %(funcName)s - %(levelname)s - %(message)s')
     syslog_handler.setFormatter(formatter)
     logger.addHandler(syslog_handler)
 
@@ -49,6 +81,7 @@ class AddHeaderMilter(libmilter.ForkMixin, libmilter.MilterProtocol):
         super(AddHeaderMilter, self).run()
 
     @libmilter.noReply
+    @handle_bytes
     def connect(self, hostname, family, ip, port, cmdDict):
         logger.info('Connect from %s:%d (%s) with family: %s', ip, port, hostname, family)
         logger.debug('cmdDict: %s', cmdDict)
@@ -57,11 +90,13 @@ class AddHeaderMilter(libmilter.ForkMixin, libmilter.MilterProtocol):
         return libmilter.CONTINUE
 
     @libmilter.noReply
+    @handle_bytes
     def helo(self, heloname):
         logger.debug('HELO: %s', heloname)
         return libmilter.CONTINUE
 
     @libmilter.noReply
+    @handle_bytes
     def mailFrom(self, frAddr, cmdDict):
         logger.debug('MAIL: %s', frAddr)
         logger.debug('cmdDict: %s', cmdDict)
@@ -69,6 +104,7 @@ class AddHeaderMilter(libmilter.ForkMixin, libmilter.MilterProtocol):
         return libmilter.CONTINUE
 
     @libmilter.noReply
+    @handle_bytes
     def rcpt(self, recip, cmdDict):
         logger.debug('RCPT: %s', recip)
         logger.debug('cmdDict: %s', cmdDict)
@@ -76,46 +112,61 @@ class AddHeaderMilter(libmilter.ForkMixin, libmilter.MilterProtocol):
         return libmilter.CONTINUE
 
     @libmilter.noReply
+    @handle_bytes
     def header(self, key, val, cmdDict):
         logger.debug('HEADER: %s: %s', key, val)
         logger.debug('cmdDict: %s', cmdDict)
         return libmilter.CONTINUE
 
     @libmilter.noReply
+    @handle_bytes
     def eoh(self, cmdDict):
         logger.debug('EOH')
         logger.debug('cmdDict: %s', cmdDict)
         return libmilter.CONTINUE
 
+    @handle_bytes
     def data(self, cmdDict):
         logger.debug('DATA')
         logger.debug('cmdDict: %s', cmdDict)
         return libmilter.CONTINUE
 
     @libmilter.noReply
+    @handle_bytes
     def body(self, chunk, cmdDict):
         logger.debug('Body chunk: %d', len(chunk))
         logger.debug('cmdDict: %s', cmdDict)
         return libmilter.CONTINUE
 
+    @handle_bytes
     def eob(self, cmdDict):
         logger.debug('EOB')
         logger.debug('cmdDict: %s', cmdDict)
-        self.message_id = cmdDict.get('i', 'unknown')
-        if self.rcpt_data['rcpt_mailer'] == 'smtp':
-            #self.setReply('554', '5.7.1', 'Rejected because I said so')
-            for action in self.config:
-                for action_type, config in action.iteritems():
-                    if action_type == 'sender':
-                        logger.debug('%s: Checking sender pattern /%s/ against address %s',
-                                     self.message_id, config['pattern'], self.mail_data['mail_addr'])
-                        if re.search(config['pattern'], self.mail_data['mail_addr']):
-                            self.addHeader(config['header_key'], config['header_value'])
-                            logger.info('%s: Added header - %s: %s', self.message_id, config['header_key'], config['header_value'])
+        try:
+            self.message_id = cmdDict.get('i', 'unknown')
+            if self.rcpt_data['rcpt_mailer'] == 'smtp':
+                #self.setReply('554', '5.7.1', 'Rejected because I said so')
+                logger.debug('Processing %d actions', len(self.config))
+                for action in self.config:
+                    for action_type, config in action.items():
+                        logger.debug('Action type: %s  Config: %s', action_type, config)
+                        if action_type == 'sender':
+                            logger.debug('%s: Checking sender pattern /%s/ against address %s',
+                                         self.message_id, config['pattern'], self.mail_data['mail_addr'])
+                            if re.search(config['pattern'], self.mail_data['mail_addr']):
+                                key = config['header_key'].encode('utf-8')
+                                val = config['header_value'].encode('utf-8')
+                                self.addHeader(key, val)
+                                logger.info('%s: Added header - %s: %s', self.message_id, key, val)
+                            else:
+                                logger.debug('%s: No match', self.message_id)
                         else:
-                            logger.debug('%s: No match', self.message_id)
-        else:
-            logger.info('%s: Skipping non-smtp delivery: %s', self.message_id, self.rcpt_data['rcpt_mailer'])
+                            logger.warn('Unhandled action type: %s', action_type)
+                logger.debug('Finished processing actions')
+            else:
+                logger.info('%s: Skipping non-smtp delivery: %s', self.message_id, self.rcpt_data['rcpt_mailer'])
+        except Exception:
+            logger.exception('Exception while processing EOB')
 
         return libmilter.CONTINUE
 
@@ -137,7 +188,7 @@ def main(config_file):
     import signal, traceback
 
     with open(config_file, 'r') as input:
-        config = yaml.load(input)
+        config = load(input, Loader=Loader)
 
     opts = {
         #'milter_opts': libmilter.SMFIF_CHGFROM | libmilter.SMFIF_ADDRCPT | libmilter.SMFIF_QUARANTINE
@@ -153,17 +204,20 @@ def main(config_file):
     #f = libmilter.ForkFactory('/var/run/add_header_milter.sock', AddHeaderMilter, opts)
 
     def sig_handler(num, frame):
+        logger.debug('Caught signal %s, calling close', num)
         f.close()
         sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGABRT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
 
     try:
         # run it
         f.run()
     except Exception as ex:
-        f.close()
         logger.exception('Exception running AddHeaderMilter')
+        f.close()
         sys.exit(3)
 
 
